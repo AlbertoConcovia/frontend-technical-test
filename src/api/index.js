5e8294fcf14c31efc12701810502fee46ac6559e
albertoconcovia <concovia@gmail.com>
2022-01-22 21:00:47 +0000

    responsive design added

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,40 @@
 // eslint-disable-next-line no-unused-vars
-import { request } from './helpers';
+import { request } from "./helpers";
 
 /**
  * Pull vehicles information
  *
- * @return {Promise<Array.<vehicleSummaryPayload>>}
+ * @return {Promise<Array.<vehiclesPrices>>}
  */
 // TODO: All API related logic should be made inside this function.
 export default async function getData() {
-  return [];
+  // Fetch the list of vehicles with summaries
+  const vehiclesResponse = await fetch('/api/vehicles.json').then(
+    (response) => {
+      return response.json();
+    }
+  );
+
+  console.log(`vehiclesResponse ${JSON.stringify(vehiclesResponse)}`);
+
+  // const vehiclesPrices = [];
+
+  // vehiclesResponse.forEach((vehicle) => {
+  //     // Get the details for the vehicle
+  //     fetch(vehicle.apiUrl)
+  //         .then((response) => {
+  //             return response.json();
+  //         })
+  //         .then((vehicleDetails) => {
+  //             // Check that the vehicle has a valid price
+  //             if (
+  //                 vehicleDetails.price !== "" &&
+  //                 vehicleDetails.price !== null
+  //             ) {
+  //                 vehiclesPrices.push(vehicle);
+  //             }
+  //         });
+  // });
+  // return vehiclesPrices;
+  return vehiclesResponse;
 }
--- src/components/VehicleList/index.js
+++ src/components/VehicleList/index.js
@@ -5,6 +5,8 @@ import './style.scss';
 export default function VehicleList() {
   // eslint-disable-next-line no-unused-vars
   const [loading, error, vehicles] = useData();
+  console.log(`index.js after vehicles ${JSON.stringify(vehicles)}`);
+  // const imgVehicle =  vehicles[0].media[0].url ;
 
   if (loading) {
     return <div data-testid="loading">Loading</div>;
@@ -16,7 +18,7 @@ export default function VehicleList() {
 
   return (
     <div data-testid="results">
-      <p>List of vehicles will be displayed here</p>
+
       <p>
         Visit
         <a href="/api/vehicles.json" target="_blank"> /api/vehicles.json</a>
@@ -35,6 +37,54 @@ export default function VehicleList() {
         {' '}
         (vehicle without any price)
       </p>
+
+      <p>List of vehicles will be displayed here</p>
+
+      <div data-testid="results" className="vehicle__container">
+
+        <div className="vehicle">
+          <div>
+            <img src="/images/16x9/fpace_k17.jpg" alt="fpace_k17" className="vehiclelist__img" />
+          </div>
+          <div>
+            <p> { JSON.stringify(vehicles[0].modelYear) } </p>
+            <p> { JSON.stringify(vehicles[0].media[0].url) } </p>
+          </div>
+        </div>
+
+        <div className="vehicle">
+          <div>
+            <img src="/images/16x9/fpace_k17.jpg" alt="fpace_k17" className="vehiclelist__img" />
+          </div>
+          <div>
+            <p> { JSON.stringify(vehicles[0].modelYear) } </p>
+            <p> { JSON.stringify(vehicles[0].media[0].url) } </p>
+          </div>
+        </div>
+
+        <div className="vehicle">
+          <div>
+            <img src="/images/16x9/fpace_k17.jpg" alt="fpace_k17" className="vehiclelist__img" />
+          </div>
+          <div>
+            <p> { JSON.stringify(vehicles[0].modelYear) } </p>
+            <p> { JSON.stringify(vehicles[0].media[0].url) } </p>
+          </div>
+        </div>
+
+
+        <div className="vehicle">
+          <div>
+            <img src="/images/16x9/fpace_k17.jpg" alt="fpace_k17" className="vehiclelist__img" />
+          </div>
+          <div>
+            <p> { JSON.stringify(vehicles[0].modelYear) } </p>
+            <p> { JSON.stringify(vehicles[0].media[0].url) } </p>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
